84bb6e1b2b01a6977459616ffabd22b877700557
Alessia0498 <A.DIFRANCESCO5@studenti.unisa.it>
2020-12-14 13:18:47 +0100

    table week in forward and add send

--- screens/forward-maintenance-activity.screen.php
+++ screens/forward-maintenance-activity.screen.php
@@ -24,6 +24,15 @@
       'week' => 3, 'note' => 'notes', 'description' => 'description'
     );
 
+    $competencies = array('competence1', 'competence2', 'competence3');
+    $maintainer =  array(
+      'name' => 'Pippo', 'skills' => '3/5', 'AM' => '80%', 'AT' => '100%',
+      'AW' => '20%', 'ATH' => '100%', 'AF' => '50%', 'ASA' => '20%', 'ASU' => '100%',
+    );
+
+
+
+
 
     echo "
         <h2 style='text-align:center'> Maintenance Activity Information </h2> 
@@ -36,9 +45,44 @@
         <td style='text-align:center;  width:10%;'>Week number: " . $data['week'] . "</td>
         </tr>
         <tr>
-        <td style='text-align:center;  width:10%;'>Skills Needed: competencies</td></tr>
+        <td style='text-align:center;  width:10%;'>
+        <p> Skills Needed: </p>
+        <li>" . $competencies[0] . "</li>
+        <li>" . $competencies[1] . "</li>
+        <li>" . $competencies[2] . "</li>
+        </td>
         <br>
-        <tr><td style='text-align:center;  width:10%;'> Maintainer Availability </td>
+        <td style='text-align:center;  width:10%;'>
+        <p> Maintainer Availability </p>
+        <table class='table1'>
+        <thead>
+        <tr>
+        <th> Maintainer </th>
+        <th> Skills </th>
+        <th> Availability  Monday</th>
+        <th> Availability  Tuesday</th>
+        <th> Availability  Wednesday</th>
+        <th> Availability  Thursday</th>
+        <th> Availability  Friday</th>
+        <th> Availability  Saturday</th>
+        <th> Availability  Sunday</th>
+        </tr>
+         </thead>
+         <tbody>
+        <tr>
+         <td style='text-align:center;  width:10%;'> " . $maintainer['name'] . "</td>
+        <td style='text-align:center;  width:10%;'> " . $maintainer['skills'] . "</td>
+        <td style='text-align:center;  width:10%;'>" . $maintainer['AM'] . "</td>
+        <td style='text-align:center;  width:10%;'>" . $maintainer['AT'] . "</td>
+        <td style='text-align:center;  width:10%;'> " . $maintainer['AW'] . "</td>
+        <td style='text-align:center;  width:10%;'> " . $maintainer['ATH'] . "</td>
+        <td style='text-align:center;  width:10%;'> " . $maintainer['AF'] . "</td>
+        <td style='text-align:center;  width:10%;'> " . $maintainer['ASA'] . "</td>
+        <td style='text-align:center;  width:10%;'> " . $maintainer['ASU'] . "</td>
+        </tr>  
+         </tbody>
+         </table>
+         </td>
         </tr></table>";
   }
   ?>
---
+++ screens/send-maintenance-activity.screen.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Send Maintenance Activity</title>
+    <meta name="author" content="gruppo 10" />
+    <link rel="stylesheet" type="text/css" href="../index.css" />
+    <meta name="content" content="Send Maintenance Activity Screen" />
+    <link rel="icon" href="../assets/service.jpg" type="image/jpg" />
+    <meta charset="utf-8" />
+</head>
+
+<body>
+
+
+</body>
+
+</html>
